30fa43d68ad56baba2098f8301e6169a0a2e736c
Pavel Turlachev <turchev@gmail.com>
2022-12-19 07:18:08 +0400

    Initial arrays

--- helpers/assembly_all.sh
+++ helpers/assembly_all.sh
@@ -3,6 +3,7 @@
 cd ../../skypro
 
 mvn clean
+mvn -pl arrays compile package
 mvn -pl helloworld compile package
 mvn -pl pro.sky:hw package
 
--- start_loops.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# helloworld module startup
-mvn -pl loops clean
-mvn -pl loops compile exec:exec
