fce3e2d75a2781fefcd1c23b82f914b4c97527b9
Pavel Turlachev <turchev@gmail.com>
2022-12-20 20:20:34 +0400

    Book and Author classes

--- objectsandclasses/src/main/java/pro/sky/objectsandclasses/Main.java
+++ objectsandclasses/src/main/java/pro/sky/objectsandclasses/Main.java
@@ -2,6 +2,18 @@ package pro.sky.objectsandclasses;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, Мир!");
+
+        Author authorLafore = new Author("Роберт", "Лафоре");
+        Author authorStevens = new Author("Эл", "Стивенс");
+
+        Book bookAlgorithmsJava = new Book("Структуры данных и алгоритмы Java", authorLafore, 2015);
+        Book bookWilleyCPlusPlus = new Book("Самоучитель по C++ от Willey", authorStevens, 2005);
+
+        System.out.println(bookAlgorithmsJava);
+        System.out.println(bookWilleyCPlusPlus);
+
+        bookAlgorithmsJava.setYearRelease(2017);
+        System.out.println(bookAlgorithmsJava);
+
     }
 }
